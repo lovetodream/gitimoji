75732b01534c9b25457699a6bb50e29a78c3b08f
Timo <38291523+TimoZacherl@users.noreply.github.com>
2020-09-22 20:41:30 +0200

    💄 - Add Settings View

---
+++ gitimoji/Views/SettingsView.swift
@@ -0,0 +1,91 @@
+//
+//  SwiftUIView.swift
+//  gitimoji
+//
+//  Created by Timo Zacherl on 22.09.20.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @ObservedObject var vm: SearchVM
+    
+    @Binding var copyEmojiBinding: Bool
+    @Binding var showSettings: Bool
+    @Binding var showAbout: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Settings").font(.headline)
+                Spacer()
+                Button(action: {
+                    self.showSettings = false
+                }, label: {
+                    Image("DismissIcon")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 16, height: 16)
+                }).buttonStyle(PlainButtonStyle())
+            }
+            Picker(selection: $copyEmojiBinding, label: Text("Value to copy")) {
+                Text("Emoji Code eg. :tada:").tag(false)
+                Text("Emoji eg. 🎉").tag(true)
+            }
+            Toggle(isOn: $vm.autoLaunchEnabled) {
+                Text("Launch App automatically")
+            }
+            Button(action: {
+                self.showAbout.toggle()
+            }, label: {
+                if showAbout {
+                    Text("Hide additional informations")
+                } else {
+                    Text("More Informations about Gitmojis")
+                }
+            }).buttonStyle(LinkButtonStyle())
+            if showAbout {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Gitmoji is an emoji guide for GitHub commit messages. Aims to be a standarization cheatsheet - guide for using emojis on GitHub's commit messages.")
+                                        
+                    Text("Using emojis on commit messages provides an easy way of identifying the purpose or intention of a commit with only looking at the emojis used.")
+                    
+                }
+                .frame(minHeight: 135)
+                .lineSpacing(1.5)
+            }
+            
+            Divider()
+            
+            HStack {
+                Button(action: {
+                    if let url = URL(string: "") {
+                    NSWorkspace.shared.open(url)
+                    }
+                }, label: {
+                    Text("Gitmoji Website")
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    if let url = URL(string: "") {
+                        NSWorkspace.shared.open(url)
+                    }
+                }) {
+                    Text("Star on GitHub")
+                }
+            }
+        }
+        .padding(10)
+        .overlay(RoundedRectangle(cornerRadius: 10.0).stroke(Color.black.opacity(0.1), lineWidth: 2.0))
+        .cornerRadius(10.0)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView(vm: SearchVM(), copyEmojiBinding: .constant(false), showSettings: .constant(true), showAbout: .constant(true))
+            .frame(width: 350)
+    }
+}
